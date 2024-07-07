a50628d5a046458ff55976664e53f43f29da00f9
sai <saisreenath.mandaloju@gmail.com>
2024-07-07 16:50:33 +0530

    .

--- src/main/java/com/example/player/controller/PlayerController.java
+++ src/main/java/com/example/player/controller/PlayerController.java
@@ -7,3 +7,26 @@
  * 
  */
 package com.example.player.controller;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.player.model.Player;
+import com.example.player.service.PlayerH2Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class PlayerController {
+    @Autowired
+    private PlayerH2Service playerH2Service;
+
+    @GetMapping("/players")
+    public ArrayList<Player> getPlayers() {
+        return playerH2Service.getPlayers();
+    }
+    
+    
+}
--- src/main/java/com/example/player/service/PlayerH2Service.java
+++ src/main/java/com/example/player/service/PlayerH2Service.java
@@ -19,12 +19,44 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import com.example.player.model.Player;
+import com.example.player.model.PlayerRowMapper;
 import com.example.player.repository.PlayerRepository;
 
 @Service
 public class PlayerH2Service implements PlayerRepository {
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private JdbcTemplate db;
+
+    @Override
+    public ArrayList<Player> getPlayers() {
+        List<Player> playerList = db.query("SELECT * FROM Player", new PlayerRowMapper());
+        ArrayList<Player> players = new ArrayList<>(playerList);
+        return players;
+    }
+
+    @Override
+    public Player getPlayerById(int playerId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPlayerById'");
+    }
+
+    @Override
+    public Player addPlayer(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addPlayer'");
+    }
+
+    @Override
+    public Player updatePlayer(int playerId, Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updatePlayer'");
+    }
+
+    @Override
+    public void deletePlayer(int playerId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletePlayer'");
+    }
     
     
 }
